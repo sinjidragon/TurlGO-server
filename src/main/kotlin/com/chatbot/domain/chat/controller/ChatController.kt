fb63651d5df3c740e61bf686208724b97145f7c8
socury <socury1104@gmail.com>
2025-03-24 15:57:02 +0900

    docs: swagger docs

--- src/main/kotlin/com/chatbot/domain/chat/controller/ChatController.kt
+++ src/main/kotlin/com/chatbot/domain/chat/controller/ChatController.kt
@@ -12,28 +12,25 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 
-@Tag(name = "ChatBot", description = "챗봇")
+@Tag(name = "쓰지마라")
 @RestController
 @RequestMapping("/chatbot")
 class ChatController (
     private val chatService: ChatService,
     private val userRepository: UserRepository
 ) {
-    @Operation(summary = "채팅 보내기")
     @PostMapping("/send")
     fun sendMessage(principal: Principal, @RequestBody request: MessageRequest): ChatMessage {
         val user: UserEntity = userRepository.findByUsername(principal.name).orElseThrow{ CustomException(UserErrorCode.USER_NOT_FOUND) }
         return chatService.sendMessage(user.id.toString(), request.message)
     }
 
-    @Operation(summary = "채팅 초기화")
     @DeleteMapping("/clear")
     fun clearHistory(principal: Principal) {
         val user: UserEntity = userRepository.findByUsername(principal.name).orElseThrow{CustomException(UserErrorCode.USER_NOT_FOUND)}
         chatService.clearHistory(user.id.toString())
     }
 
-    @Operation(summary = "채팅기록 조회하기")
     @GetMapping("/history")
     fun getHistory(principal: Principal): List<ChatMessage> {
         val user: UserEntity = userRepository.findByUsername(principal.name).orElseThrow{CustomException(UserErrorCode.USER_NOT_FOUND)}
